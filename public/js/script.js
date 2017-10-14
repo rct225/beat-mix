49e52e5f9b8eb72b9d7bfc4f461b47df959a5b8d
Robert Tuck <robtuck@gmail.com>
2017-10-14 14:13:57 -0400

    added getNeighborPads()

--- public/js/script.js
+++ public/js/script.js
@@ -54,3 +54,80 @@ function invert(drumsArrayName) {
   }
 
 }
+
+function getNeighborPads(x, y, size) {
+  if (x >= size || y >= size) {
+    return [];
+  }
+
+  if (x < 0 || y < 0) {
+    return [];
+  }
+
+  if (x == 0 && y == 0) {
+    return [
+      [0,1],
+      [1,0]
+    ];
+  }
+
+  if (x == size - 1 && y == size - 1) {
+    return [
+      [x, y - 1],
+      [x - 1, y]
+    ];
+  }
+
+  if (x == size - 1 && y == 0) {
+    return [
+      [x, 1],
+      [x - 1, 0]
+    ];
+  }
+
+  if (x == 0 && y == size - 1) {
+    return [
+      [0, y - 1],
+      [1, y]
+    ];
+  }
+
+  if (x == 0) {
+    return [
+      [0, y - 1],
+      [0, y + 1],
+      [1, y]
+    ];
+  }
+
+  if (x == size - 1) {
+    return [
+      [x, y - 1],
+      [x, y + 1],
+      [x - 1, y]
+    ];
+  }
+
+  if ( y == 0 ) {
+    return [
+      [x, y + 1],
+      [x + 1, y],
+      [x - 1, y]
+    ];
+  }
+
+  if ( y == size - 1) {
+    return [
+      [x, y - 1],
+      [x + 1, y],
+      [x - 1, y]
+    ];
+  }
+  
+  return [
+    [x - 1, y],
+    [x + 1, y],
+    [x, y + 1],
+    [x, y - 1]
+  ];
+}
