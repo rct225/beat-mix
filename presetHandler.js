6e757ed615b691f8e9dd9f21b14651c37416e352
Robert Tuck <robtuck@gmail.com>
2017-10-14 12:56:42 -0400

    modified presetHandler fucntion().  Implemented POST portion.

--- presetHandler.js
+++ presetHandler.js
@@ -3,13 +3,14 @@ const presets = require('./presets');
 
 // Complete this function:
 const presetHandler = (method, presetsIndex, newPresetArray) => {
+  if (presetsIndex < 0 || presetsIndex > presets.length - 1) {
+    return [404]
+  }
   if (method == 'GET') {
-    if (presetsIndex < 0 || presetsIndex > presets.length - 1) {
-      return [404]
-    }
     return [200].concat([presets[presetsIndex]]);
   } else if (method == 'PUT') {
-
+    presets[presetsIndex] = newPresetArray;
+    return [200].concat([presets[presetsIndex]]);
   } else {
     return [400];
   }
