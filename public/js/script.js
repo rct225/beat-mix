78447a2c484f0a79df0259a5e0f4a9f516a74fe2
Robert Tuck <robtuck@gmail.com>
2017-10-14 11:48:04 -0400

    toggleDrum() function added

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,21 @@ let kicks = new Array(16).fill(false);
 let snares = new Array(16).fill(false);
 let hiHats = new Array(16).fill(false);
 let rideCymbals = new Array(16).fill(false);
+
+function toggleDrum(drums, drumIndex) {
+  if (!drums) {
+    return;
+  }
+
+  if (!['kicks', 'snares', 'hiHats', 'rideCymbals'].includes(drums)) {
+    return;
+  }
+
+  if (drumIndex < 0 || drumIndex > drums.length - 1) {
+    return;
+  }
+
+  drumsArray = eval(drums);
+
+  drumsArray[drumIndex] = !(drumsArray[drumIndex])
+}
