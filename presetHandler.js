b60173725ac6b5933b5f586219b52b57e48a7d56
Robert Tuck <robtuck@gmail.com>
2017-10-14 12:47:28 -0400

    created presetHandler fucntion().  Implemented GET portion. '

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,18 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, presetsIndex, newPresetArray) => {
+  if (method == 'GET') {
+    if (presetsIndex < 0 || presetsIndex > presets.length - 1) {
+      return [404]
+    }
+    return [200].concat([presets[presetsIndex]]);
+  } else if (method == 'PUT') {
+
+  } else {
+    return [400];
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
